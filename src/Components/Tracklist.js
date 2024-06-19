f651a324b848072e54a4a6eba061e0abcfebb1f7
BrayanCordero <corderobrayan@yahoo.com>
2024-06-18 20:23:55 -0700

    refactor mapping of tracks

--- src/Components/Tracklist.js
+++ src/Components/Tracklist.js
@@ -2,14 +2,18 @@ import React from "react";
 import Track from "./Track"
 
 function TrackList(props){
+
+    const track = props.result.map(track =>{
+        return(<li key={track.id}>
+            <Track track={track}/>
+                </li>)
+    })
     
     return(
         <div>
-            {props.result.map((track) => {
-                return(
-                    <Track key={track.id} track={track} />
-                )
-            })}
+            <ul style={{listStyle:"none"}}>
+                {track}
+            </ul>
         </div>
         
     )
